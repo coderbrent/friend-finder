d598405a37d61a9baa7b8c927f23c66e2d8dcd92
Brent Abruzese <brent.abruzese@gmail.com>
2019-05-17 21:59:33 -0400

    deleted some test files and built the landing page

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+app.get('/survey', function(req, res) {
+  res.send('nada');
+})
+
+app.get('/', function(req, res) {
+  res.send('what!');
+})
+
+app.get('/', function(req, res) {
+
+})
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const mysql = require('mysql');
+const port = process.env.PORT || 8080;
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/survey', function(req, res) {
+
+})
+
+app.get('/', function(req, res) {
+  res.send('what!');
+})
