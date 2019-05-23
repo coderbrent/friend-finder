a87cfe5b037cb33aa5b8718969567bcd7812a85e
Brent Abruzese <brent.abruzese@gmail.com>
2019-05-22 20:43:26 -0400

    ton of changes - lots of trial and error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,12 @@
+const express = require('express');
+const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 
 module.exports = function(app) {
 
-var friends = require("/Users/brentabruzese/friend-finder/app/data/friends.json")
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/api/friends', function(req, res) {
-  const keys = Object.values(friends);
-  for(const key of keys){
-    res.send(keys);
-  }
-})
-
-app.post('/api/friends', function(req, res) {
-  json = res.json(req.body);
-  res.send(json);
-  console.log(json);
-})
+  app.get('/api/friends', function(req, res) {
+    const jsonData = require('../data/friends.json')
+    res.send(jsonData);
+  });
 
 }
-
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
-const path = require('path');
 
 const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('app/public'));
+app.use(express.static('/public'));
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(port, () => console.log(`Friend Finder app listening on port ${port}!`))
 
