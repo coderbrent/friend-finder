2b82d40cc9845a353181a7bff1f4891d50e75653
Brent Abruzese <brent.abruzese@gmail.com>
2019-06-20 15:31:38 -0400

    see commit message for details

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+module.exports = [
+{
+  name: "Brent Abruzese",
+  photo: "",
+  scores: [3, 3, 3, 3, 3, 3, 5, 1, 1, 3]
+  },
+  {
+  name: "Mike Cafiero",
+  photo: "",
+  scores: [1, 1, 4, 2, 3, 1, 1, 1, 1, 2]
+  },
+  {
+  name: "Evan Battista",
+  photo: "",
+  scores: [2, 2, 4, 1, 5, 1, 3, 2, 1, 1]
+  },
+  {
+  name: "Nick Peterpaul",
+  photo: "",
+  scores: [3, 4, 5, 2, 4, 5, 5, 5, 1, 3]
+  },
+  {
+  name: "Chris Schirripa",
+  photo: "",
+  scores: [3, 4, 2, 2, 4, 5, 5, 3, 3, 3]
+  },
+  {
+  name: "Gary Cunningham",
+  photo: "https://scontent.fewr1-4.fna.fbcdn.net/v/t1.0-9/12243255_10207936347675002_9079617410681595472_n.jpg?_nc_cat=104&_nc_oc=AQl-DqmTok34C4oh2SAuvxFaSOXmYNUpDGz7-3Vou6OyQs0ar4Wf4vlseQQLAuH3_x4&_nc_ht=scontent.fewr1-4.fna&oh=545b83055981e82bbeae32a180227071&oe=5D791803",
+  scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+  name: "Stevie Baum",
+  photo: "",
+  scores: [2, 2, 2, 2, 4, 5, 5, 3, 1, 3]
+  },
+  {
+  name: "Dominick DeCoco",
+  photo: "",
+  scores: [3, 4, 3, 2, 4, 5, 5, 1, 1, 3]
+  },
+  {
+  name: "Antonio Margheriti",
+  photo: "",
+  scores: [3, 3, 1, 2, 4, 5, 5, 4, 1, 3]
+  },
+  {
+  name: "Enzo Gorlami",
+  photo: "",
+  scores: [1, 1, 2, 3, 5, 5, 5, 4, 1, 3]
+  }
+]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const friends = require('../data/friends.js')
 
-module.exports = function(app) {
+module.exports = app => {
+  app.post('/api/friends', (req, res) => {
+    const userScore = req.body.scoreTotal;
+    const scoreArray = [];
+    //iterate through the imported friends array, tally up all their scores and put them into an array.
+    for(i = 0; i < friends.length; i++) {
+      
+      //helper function to pass to the reduceRight method used when creating our new array below.
+      const arrSum = (total, value) => {
+        return total + value;
+      }
 
-  app.get('/api/friends', function(req, res) {
-    const jsonData = require('../data/friends.json')
-    res.send(jsonData);
+      friendName = friends[i].name;
+      photoURL = friends[i].photo;
+      //gets the stored friends scores, sums them up and then subtracts them from the submitted users score.
+      //since i was getting negative numbers here, i used Math.abs to make sure that "0" was the smallest number we could get.
+      scoreSum = Math.abs(userScore - friends[i].scores.reduceRight(arrSum));
+      scoreArray.push([friendName, photoURL, scoreSum])  
+    }
+
+    //since we know the smallest number (difference) will indicate our closest match, we sort the scoreArray in descending order by
+    //the value in it's second position (which, in this case, is the users difference score)
+    scoreArray.sort((a,b) => {
+      return a[2] - b[2]
+    })
+
+    //since the array is now sorted properly, our best match will sit in the 0 position which can be easily extracted.
+    let match = scoreArray[0];    
+    console.log(match);
+    friends.push(req.body)
+    res.end();
+    
   });
+    
 
+  
+  app.get('/api/friends', (req, res) => {
+
+  })
 }
